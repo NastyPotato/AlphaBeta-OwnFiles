2066d18867136deb0d27203f5909e2bb834a05eb
NastyPotato <56434135+NastyPotato@users.noreply.github.com>
2021-05-04 21:31:25 +0300

    Add files via upload

---
+++ Assets/Scripts/AnimationControllerTestingScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerTestingScript : MonoBehaviour
+{
+    CharacterController controller;
+
+    Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller=GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the script tells the spider to walk
+        if(Input.GetKey(KeyCode.Q))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        //When the script tells the spider to attack
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        //When the script tells the spider to die
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetInteger("State", 3);
+        }
+
+        //When the script tells the spider to idle
+        if (Input.GetKey(KeyCode.E))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+}
